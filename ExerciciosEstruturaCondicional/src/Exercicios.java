89a81999dff9a473697b3a352d5899390ac289b8
slnntk <iamsunli155@gmail.com>
2022-11-04 11:17:43 -0300

    Testes

--- ExerciciosEstruturaCondicional/src/Exercicios.java
+++ ExerciciosEstruturaCondicional/src/Exercicios.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercicios {
@@ -32,7 +33,7 @@ public class Exercicios {
         double B = sc.nextDouble();
 
        if (A % B == 0 || B % A == 0){
-           System.out.printf("São Multiplos");
+           System.out.println("São Multiplos");
        }
        else{
            System.out.println("Não são multiplos");
@@ -54,7 +55,122 @@ public class Exercicios {
         }
     }
 
-    
+    public void Exercicio05(){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        int item = sc.nextInt();
+        int quantidade = sc.nextInt();
+        
+        double valor = 0;
+        if (item == 1){
+            valor =  4 * quantidade;
+        }
+        else if (item == 2){
+            valor =  4.50 * quantidade;
+        }
+        else if (item == 3){
+            valor =  5 * quantidade;
+        }
+        else if (item == 4){
+            valor =  2 * quantidade;
+        }
+        else if (item == 5){
+            valor = 1.5 * quantidade;
+        }
+        else{
+            System.out.println("PRODUTO INDISPONIVEL");
+        }
+        sc.close();
+
+        System.out.printf("\nTotal: R$%.2f", valor);
+    }
+
+    public void Exercicio06(){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double intervalo = sc.nextDouble();
+
+        if (intervalo  < 0 || intervalo > 100){
+            System.out.println("Fora de intervalo.");
+        }
+        else if (intervalo <= 25){
+            System.out.println("Intervalo: " + "(0,25]");
+        }
+        else if (intervalo <= 50){
+            System.out.println("Intervalo: " + "(25,50]");
+        }
+        else if (intervalo <= 75){
+            System.out.println("Intervalo: " + "(50,75]");
+        }
+        else if (intervalo <= 100){
+            System.out.println("Intervalo: " + "(75,100]");
+        }
+    }
+
+    public void Exercicio07(){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        double x = sc.nextDouble();
+        double y = sc.nextDouble();
+
+        if (x > 0 && y > 0){
+            System.out.println("Q1");
+        }
+        else if (x < 0 && y > 0){
+            System.out.println("Q2");
+        }
+        else if (x < 0 && y < 0){
+            System.out.println("Q3");
+        }
+        else if (x > 0 && y < 0){
+            System.out.println("Q4");
+        }
+        else{
+            System.out.println("Origem");
+        }
+    }
+
+    public void Exercicio08(){
+
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+        double renda = sc.nextDouble();
+        double imposto = 0;
+
+        if (renda <= 2000){
+            imposto = 0;
+        }
+        else if (renda <= 3000){
+            imposto = (renda - 2000) * 0.08;
+        }
+        else if (renda <= 4500){
+            imposto = (renda - 3000) * 0.18 + 1000 * 0.08;
+        }
+        else if (renda > 4500){
+            imposto = (renda - 4500) * 0.28 + 1500 * 0.18 + 1000 * 0.08;
+        }
+
+        if (imposto == 0.0){
+            System.out.println("Isento");
+        }
+        else {
+            System.out.printf("R$: %.2f", imposto);
+        }
+
+
+    }
+
+
 
 
 
--- ExerciciosEstruturaCondicional/src/Main.java
+++ ExerciciosEstruturaCondicional/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Exercicios e1 = new Exercicios();
-        e1.Exercicio04();
+        e1.Exercicio08();
 
     }
 }
